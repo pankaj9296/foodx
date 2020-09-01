7a5dab289e3323ac7d099fa6ca19eca1983198d4
Nishant Desai <nishantdesai1306@gmail.com>
2020-09-01 15:12:11 +0530

    modified product model

--- packages/api/shop/services/product/product.model.ts
+++ packages/api/shop/services/product/product.model.ts
@@ -5,6 +5,7 @@ const Product = sequelize.define('Product', {
 	id: {
 		type: DataTypes.BIGINT,
 		allowNull: false,
+		autoIncrement: true,
 		primaryKey: true,
 	},
 	slug: {
@@ -12,38 +13,34 @@ const Product = sequelize.define('Product', {
 		allowNull: false
 	},
 	title: {
-		type: DataTypes.STRING,
+		type: DataTypes.STRING(1024),
 		allowNull: false
 	},
 	image: {
-		type: DataTypes.STRING,
+		type: DataTypes.STRING(1024),
 		allowNull: false
 	},
 	description: {
-		type: DataTypes.STRING,
+		type: DataTypes.STRING(5000),
 		allowNull: false
 	},
 	price: {
-		type: DataTypes.BIGINT,
+		type: DataTypes.DOUBLE,
 		allowNull: false
 	},
 	salePrice: {
-		type: DataTypes.BIGINT,
+		type: DataTypes.DOUBLE,
 		allowNull: false
 	},
 	discountInPercent: {
-		type: DataTypes.BIGINT,
+		type: DataTypes.DOUBLE,
 		allowNull: true
 	},
-	weight: {
+	unit: {
 		type: DataTypes.STRING,
 		allowNull: false
 	},
 	quantity: {
-		type: DataTypes.BIGINT,
-		allowNull: false
-	},
-	total: {
 		type: DataTypes.BIGINT,
 		allowNull: false
 	},
