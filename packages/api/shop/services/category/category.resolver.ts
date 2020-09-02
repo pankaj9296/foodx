0e063103f2ebabb30a211fa7fb9e9ca832106995
Nishant Desai <nishantdesai1306@gmail.com>
2020-09-02 10:12:03 +0530

    fetch data from db for category resolver

--- packages/api/shop/services/category/category.resolver.ts
+++ packages/api/shop/services/category/category.resolver.ts
@@ -1,6 +1,9 @@
 import { Resolver, Query, Arg, Int } from 'type-graphql';
+import ArrayToTree from 'array-to-tree';
+import { Model } from 'sequelize';
 import loadCategories from './category.sample';
 import Category from './category.type';
+import Models from '../models';
 
 @Resolver()
 export class CategoryResolver {
@@ -9,15 +12,47 @@ export class CategoryResolver {
   @Query(() => [Category], { description: 'Get all the categories' })
   async categories(
     @Arg('type', type => String) type: string
-  ): Promise<Category[]> {
-    return await this.items.filter(item => item.type === type);
-    // return await this.items;
+  ): Promise<any[]> {
+    const rootCategories = await Models.CategoryModel.findAll({
+      where: {
+        type
+      }
+    });
+
+    const rootCategoriesJSONArray = rootCategories.map((rootCategory) => rootCategory.toJSON());
+
+    return ArrayToTree(
+      rootCategoriesJSONArray,
+      {
+        customID: 'id',
+        parentProperty: "CategoryId"
+      }
+    );
   }
 
   @Query(() => Category)
   async category(
     @Arg('id', type => Int) id: number
-  ): Promise<Category | undefined> {
+  ): Promise<any> {
+    const rootCategory: any = await Models.CategoryModel.findOne({
+      where: {
+        id,
+      }
+    });
+
+    if (rootCategory) {
+      const rootCategoryJSON = rootCategory.toJSON();
+      const childCategories = await rootCategory.getCategories();
+      const childCategoriesJSON = childCategories.map((cc: any) => cc.toJSON());
+
+      rootCategoryJSON.children = childCategoriesJSON;
+
+      return rootCategoryJSON;
+    }
+    else {
+      return null;
+    }
+
     return await this.items.find(item => item.id === id);
   }
 }
